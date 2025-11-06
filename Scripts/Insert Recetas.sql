61a86dfc689b3e7f7067d76addd2cdbcc5aa4c08
Ignacio Montaldi <87707566+ignacio-montaldi@users.noreply.github.com>
2025-11-06 16:12:34 -0300

    movi archivos

--- Scripts/Insert Recetas.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO recipes (title, subtitle, user_id)
-VALUES (
-  'Cinnamon Rolls',
-  'Rollos de canela, con azucar negra y cubiertos con crema de vainilla',
-  now(),
-  '273b5634-f824-4d8c-99e3-5850907111d1'
-)
-RETURNING id;
-
