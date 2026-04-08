d1d012fba99a40aa77d8398f91b8d8426925c415
Ignacio Montaldi <87707566+ignacio-montaldi@users.noreply.github.com>
2026-04-08 15:27:03 -0300

    feature grupos final commit

--- lib/global/enviroment.dart
+++ lib/global/enviroment.dart
@@ -12,14 +12,10 @@ class Enviroment {
 
   static String baseUrlProd = 'https://allowing-starling-heroic.ngrok-free.app';
 
-  static String apiUrl = '$baseUrlDev/api';
+  //Cambiar estas si se está en prod o en dev
+  static String apiUrl = '$baseUrlProd/api';
+  static String uploadsUrl = baseUrlProd;
 
-  static String get uploadsUrl {
-    if (Platform.isAndroid) {
-      return 'http://10.0.2.2:3000';
-    } else if (Platform.isIOS) {
-      return 'http://localhost:3000';
-    }
-    return 'http://localhost:3000';
-  }
+  // static String apiUrl = '$baseUrlDev/api';
+  // static String uploadsUrl = baseUrlDev;
 }
