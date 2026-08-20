00a9da9b811894add42cc43df111e363a939a24e
Ignacio Montaldi <87707566+ignacio-montaldi@users.noreply.github.com>
2026-08-20 19:05:29 -0300

    creado de configuración de launch.json

--- lib/global/enviroment.dart
+++ lib/global/enviroment.dart
@@ -1,21 +1,4 @@
-import 'dart:io';
-
 class Enviroment {
-  static String get baseUrlDev {
-    if (Platform.isAndroid) {
-      return 'http://10.0.2.2:3000';
-    } else if (Platform.isIOS) {
-      return 'http://localhost:3000';
-    }
-    return 'http://localhost:3000';
-  }
-
-  static String baseUrlProd = 'https://allowing-starling-heroic.ngrok-free.app';
-
-  //Cambiar estas si se está en prod o en dev
-  // static String apiUrl = '$baseUrlProd/api';
-  // static String uploadsUrl = baseUrlProd;
-
-  static String apiUrl = '$baseUrlDev/api';
-  static String uploadsUrl = baseUrlDev;
+  static const String apiUrl = String.fromEnvironment('API_URL');
+  static const String uploadsUrl = String.fromEnvironment('UPLOADS_URL');
 }
